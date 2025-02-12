a7f908809a942de43eb7c010fa49d0dd0af24962
Rana Salem <ranatasalem@gmail.com>
2025-02-12 15:22:44 +0000

    reformat router.js

--- src/api/example/helpers/find-all-example-data.js
+++
@@ -1,9 +0,0 @@
-const findAllExampleData = (db) => {
-  const cursor = db
-    .collection('example-data')
-    .find({}, { projection: { _id: 0 } })
-
-  return cursor.toArray()
-}
-
-export { findAllExampleData }
--- src/api/router.js
+++ src/api/router.js
@@ -1,13 +1,10 @@
 import { health } from '~/src/api/health/index.js'
-import { example } from '~/src/api/example/index.js'
 
 const router = {
   plugin: {
     name: 'Router',
     register: async (server) => {
       await server.register([health])
-
-      await server.register([example])
     }
   }
 }
