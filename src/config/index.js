3ccc12bd55fbe6653747949b21dc4d104d8eb443
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-02-19 13:20:38 +0000

    Add nullable prop to AWS config

--- src/config/index.js
+++ src/config/index.js
@@ -125,24 +125,26 @@ const config = convict({
       doc: 'AWS Endpoint (for LocalStack)',
       format: String,
       default: null,
-      env: 'LOCALSTACK_ENDPOINT'
+      env: 'SQS_ENDPOINT'
     },
     region: {
       doc: 'AWS Region',
       format: String,
-      default: 'eu-west-1',
+      default: 'eu-west-2',
       env: 'AWS_REGION'
     },
     accessKeyId: {
       doc: 'AWS Access Key ID',
       format: String,
       default: null,
+      nullable: true,
       env: 'AWS_ACCESS_KEY_ID'
     },
     secretAccessKey: {
       doc: 'AWS Secret Access Key',
       format: String,
       default: null,
+      nullable: true,
       env: 'AWS_SECRET_ACCESS_KEY'
     }
   },
