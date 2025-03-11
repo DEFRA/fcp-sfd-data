c10433bfcc4def1aeb47b825de082664a9319349
Adam Kay <thousand_leaves@hotmail.com>
2025-03-11 15:07:30 +0000

    rename test and add coverage

--- test/integration/data/db.test.js
+++ test/integration/data/db.test.js
@@ -1,11 +1,16 @@
 import { describe, test, expect } from '@jest/globals'
-import { db } from '../../../src/data/index.js'
+import { db, client } from '../../../src/data/index.js'
 
 describe('Create Mongo client', () => {
   test('should return an instance of database client', async () => {
     expect(db).toBeDefined()
     expect(db.s.namespace.db).toBe('fcp-sfd-data')
     expect(db.databaseName).toBe('fcp-sfd-data')
+  })
+
+  test('should have a connected MongoDB client', async () => {
+    expect(client).toBeDefined()
+    expect(client.topology.isConnected()).toBe(true)
   })
 
   test('db client should be able to upload data to collection', async () => {
