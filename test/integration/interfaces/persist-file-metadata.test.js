98ce9ececb97db75feac512f913ac206519ff86b
Adam Kay <thousand_leaves@hotmail.com>
2025-03-11 09:24:47 +0000

    updates tests to use mock data

--- test/integration/interfaces/persist-file-metadata.test.js
+++ test/integration/interfaces/persist-file-metadata.test.js
@@ -1,6 +1,8 @@
-import { describe, test, expect, beforeEach, jest } from '@jest/globals'
+import { jest, describe, test, expect, beforeEach } from '@jest/globals'
 import { persistFileMetadata } from '../../../src/interfaces/persist-inbound.js'
 import { fileMetadataCollection, notificationsCollection } from '../../../src/data/index.js'
+
+import v1FileMetadata from '../../mocks/file-metadata/v1.js'
 
 describe('Persist notifications to db', () => {
   beforeEach(async () => {
@@ -9,12 +11,12 @@ describe('Persist notifications to db', () => {
   })
 
   test('should persist a record in the fileMetadata collection', async () => {
-    await persistFileMetadata({ testKey: 'test-value' })
+    await persistFileMetadata(v1FileMetadata)
     const result = await fileMetadataCollection.find().toArray()
 
     expect(result).toBeDefined()
     expect(result.length).toBe(1)
-    expect(result[0].testKey).toBe('test-value')
+    expect(result[0]).toEqual(v1FileMetadata)
   })
 
   test('should throw an error if persisting file metadata fails', async () => {
@@ -23,7 +25,7 @@ describe('Persist notifications to db', () => {
     })
 
     try {
-      await persistFileMetadata({ testKey: 'test-value' })
+      await persistFileMetadata(v1FileMetadata)
     } catch (error) {
       expect(error.message).toBe('Error while persisting file metadata: Database error')
     }
--- test/integration/interfaces/persist-notifications.test.js
+++ test/integration/interfaces/persist-notifications.test.js
@@ -2,6 +2,8 @@ import { describe, test, expect, beforeEach, jest } from '@jest/globals'
 import { persistCommsNotification } from '../../../src/interfaces/persist-inbound.js'
 import { notificationsCollection, fileMetadataCollection } from '../../../src/data/index.js'
 
+import v1CommsMessage from '../../mocks/comms-message/v1.js'
+
 describe('Persist notifications to db', () => {
   beforeEach(async () => {
     await notificationsCollection.deleteMany({})
@@ -9,12 +11,12 @@ describe('Persist notifications to db', () => {
   })
 
   test('should persist a record in the notifications collection', async () => {
-    await persistCommsNotification({ testKey: 'test-value' })
+    await persistCommsNotification(v1CommsMessage)
     const result = await notificationsCollection.find().toArray()
 
     expect(result).toBeDefined()
     expect(result.length).toBe(1)
-    expect(result[0].testKey).toBe('test-value')
+    expect(result[0]).toEqual(v1CommsMessage)
   })
 
   test('should throw an error if persisting comms notification fails', async () => {
@@ -23,7 +25,7 @@ describe('Persist notifications to db', () => {
     })
 
     try {
-      await persistCommsNotification({ testKey: 'test-value' })
+      await persistCommsNotification(v1CommsMessage)
     } catch (error) {
       expect(error.message).toBe('Error while persisting comms notification: Database error')
     }
