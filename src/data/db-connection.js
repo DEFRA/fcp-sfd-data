2e4b254591073d388add6e3a058d1714ed8f3390
Simon Dunn <simondunn90@gmail.com>
2025-03-12 20:35:59 +0000

    remove log

--- src/data/db-connection.js
+++ src/data/db-connection.js
@@ -1,10 +1,6 @@
 import { MongoClient } from 'mongodb'
 import { config } from '../config/index.js'
 
-// urlToHttpOptions returns undefined
-console.log('DOCKER_TEST env', process.env.DOCKER_TEST)
-console.log('MONGO_URI env', process.env.MONGO_URI)
-console.log('MONGO_URI_LOCAL env', process.env.MONGO_URI_LOCAL)
 const url = config.get('mongo.urlToHttpOptions')
 
 const client = new MongoClient(url)
@@ -13,8 +9,6 @@ const db = client.db(config.get('mongo.database'))
 
 const notificationsCollection = await db.collection('notifications')
 const fileMetadataCollection = await db.collection('fileMetadata')
-
-console.log('MongoDB connected')
 
 export {
   db,
