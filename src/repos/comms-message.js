79eb447411df501c2e2e6343253e08b31e711eea
Simon Dunn <simondunn90@gmail.com>
2025-03-20 16:18:38 +0000

    refactor comms message to group events by correlationId
    Co-authored-by: Adam Kay <thousand-leaves@users.noreply.github.com>

--- src/repos/comms-message.js
+++ src/repos/comms-message.js
@@ -5,7 +5,25 @@ const notificationsCollection = config.get('mongo.collections.notifications')
 
 const persistCommsNotification = async (notification) => {
   try {
-    await db.collection(notificationsCollection).insertOne(notification)
+    const correlationId = notification.commsMessage.data.correlationId
+
+    const existingDocument = await db.collection(notificationsCollection).findOne({ _id: correlationId })
+
+    if (existingDocument) {
+      await db.collection(notificationsCollection).updateOne(
+        { _id: correlationId },
+        { $push: { events: notification } }
+      )
+    } else {
+      await db.collection(notificationsCollection).insertOne({ _id: correlationId, events: [notification] })
+    }
+
+    // if document found append the new notification to the existing document in the events array
+    // await db.collection(notificationsCollection).updateOne(
+    //   { _id: notification.commsMessage.data.correlationId },
+    //   { $push: { events: notification } },
+    //   { upsert: true }
+    // )
   } catch (error) {
     throw new Error(`Error while persisting comms notification: ${error.message}`)
   }
--- test/integration/repos/comms-message.test.js
+++ test/integration/repos/comms-message.test.js
@@ -21,14 +21,24 @@ describe('Persist inbound messages to db', () => {
       expect(result.length).toBe(1)
 
       const savedNotification = result[0]
-      expect(savedNotification).toMatchObject(v1CommsMessage)
+      expect(savedNotification.events[0]).toMatchObject(v1CommsMessage)
     })
 
-    test('should throw error when trying to insert duplicate _id', async () => {
+    test('should not create a new record when correlationId is the same as an existing document _id', async () => {
+      console.log(v1CommsMessage)
       await persistCommsNotification(v1CommsMessage)
-      await expect(persistCommsNotification(v1CommsMessage))
-        .rejects
-        .toThrow('Error while persisting comms notification')
+      const correlatedV1CommsMessage = { ...v1CommsMessage, id: 'f47ac10b-58cc-4372-a567-0e02b2c3d479' }
+      await persistCommsNotification(correlatedV1CommsMessage)
+
+      const result = await db.collection(notificationsCollection).find({ _id: v1CommsMessage.commsMessage.data.correlationId }).toArray()
+
+      expect(result).toBeDefined()
+      expect(result.length).toBe(1)
+      expect(result[0].events[0]).toMatchObject(v1CommsMessage)
+      expect(result[0].events[1]).toMatchObject(correlatedV1CommsMessage)
     })
+
+    // TODO add test for error handling if correlation id is null
+    // TODO add test to check event with different values but same correlation id are saved in the same document
   })
 })
