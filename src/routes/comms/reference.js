9a91c72acd082f5d477dc01764a773049f371bec
Simon Dunn <simondunn90@gmail.com>
2025-05-12 20:41:14 +0100

    add reference route to documentation

--- src/routes/comms/reference.js
+++ src/routes/comms/reference.js
@@ -1,30 +1,37 @@
 import Boom from '@hapi/boom'
 import Joi from 'joi'
 
-import { getCommsEventByReference } from '../../repos/comms/comms-message.js'
 import { NotFoundError } from '../../errors/not-found-error.js'
+import { httpStatusResult } from '../../schemas/common/response.js'
+import { successModel } from '../../schemas/comms/responses/reference.js'
+import { failAction } from '../../api/common/helpers/fail-action.js'
+import { getCommsEventByReference } from '../../repos/comms/comms-message.js'
 
 export default [{
   method: 'GET',
   path: '/api/v1/comms/events/reference/{reference}',
   options: {
+    description: 'Returns an array of commsEvents by reference',
+    tags: ['api', 'comms'],
+    plugins: { 'hapi-swagger': httpStatusResult(successModel) },
     auth: false,
     validate: {
       params: Joi.object({
         reference: Joi.string().required()
-      })
-    }
-  },
-  handler: async (request, h) => {
-    try {
-      const { reference } = request.params
-      const data = await getCommsEventByReference(reference)
-      return h.response({ data })
-    } catch (err) {
-      if (err instanceof NotFoundError) {
-        throw Boom.notFound(err)
+      }),
+      failAction
+    },
+    handler: async (request, h) => {
+      try {
+        const { reference } = request.params
+        const data = await getCommsEventByReference(reference)
+        return h.response({ data })
+      } catch (err) {
+        if (err instanceof NotFoundError) {
+          throw Boom.notFound(err)
+        }
+        throw Boom.internal(err)
       }
-      throw Boom.internal(err)
     }
   }
 }]
